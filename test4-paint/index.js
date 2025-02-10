69e4da0a402199d03f39ba4f4c002c3b05761400
WendySJW <wendy.shijia@gmail.com>
2025-02-10 18:45:51 +1300

    update

--- test4-paint/index.js
+++ test4-paint/index.js
@@ -7,7 +7,7 @@ import { EffectComposer } from 'jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'jsm/postprocessing/RenderPass.js';
 
 
-const model = 'assets/valley-1.glb'
+const model = 'assets/valley-3.glb'
 
 // const BGcolor = 0xFECDAC
 const BGcolor = 0xFEE7C2
